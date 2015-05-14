3cc4feb3258f149ff05e7d585726ac62cc242f35
Ramona Bellamy <bellamy2727@yahoo.com>
2015-05-13 21:07:04 -0500

    final

--- dist/main.js
+++ dist/main.js
@@ -10,34 +10,41 @@ function* guessingGame() {
 	var userNum;
 	var random;
 	var bool = true;
+	var answer;
+
 
 	
-	random = Math.ceil(Math.random()* 100);
-	console.log('random ' + random);
+	
+		
+		random = Math.ceil(Math.random()* 100);
+		console.log('random ' + random);
 
-	do{
-		userNum = yield game.ask('Choose a number between 1 and 100.');
-		userNum = parseInt(userNum);
+		do{
+			userNum = yield game.ask('Choose a number between 1 and 100.');
+			userNum = parseInt(userNum);
 
-	}while(userNum < 1 || userNum > 100);
+		}while(userNum < 1 || userNum > 100);
 
-	while(bool){
-		// console.log('random ' + random);
-		// //console.log('userNum ' + userNum);
-		userNum = parseInt(userNum);
+		while(bool){
+			// console.log('random ' + random);
+			// //console.log('userNum ' + userNum);
+			userNum = parseInt(userNum);
 
-		if(userNum === random) {	
-			bool = false;
-			console.log('correct answer');
+			if(userNum === random) {	
+				bool = false;
+				// console.log('correct answer');
+			}
+			else if(userNum < random ){
+				userNum = yield game.ask( 'Your number is too low. Please guess again.');
+			}
+			else if(userNum > random){
+				userNum = yield game.ask( 'Your number is too high. Please guess again.');
+			}
 		}
-		else if(userNum < random ){
-			userNum = yield game.ask( 'Your number is too low. Please guess again.');
-		}
-		else if(userNum > random){
-			userNum = yield game.ask( 'Your number is too high. Please guess again.');
-		}
-	}
+		
 
-	console.log(game.say('You guessed the correct number!'));
+
+		console.log(yield game.say('You Guessed Correctly! '));
+	
 
 }
